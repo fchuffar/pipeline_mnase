28d46b5b3ddefdd2e331bf229c86a2019ab10d6b
Florent Chuffart <florent.chuffart@gmail.com>
2020-11-12 21:05:18 +0100

    Tunning wf for  GSE101597

--- 01_preprocessing.sh
+++ 01_preprocessing.sh
@@ -5,22 +5,25 @@ echo $project
 rsync -auvP ~/projects/${project}/results/${gse}/ dahu:~/projects/${project}/results/${gse}/
 ## data description
 echo https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=${gse}
-# GSM2706541  Input_R1
-# GSM2706542  H3K9ac_R1
-# GSM2706543  H3K14pr_R1
-# GSM2706544  H3K14bu_R1
-# GSM2706545  Input_R2
-# GSM2706546  H3K9ac_R2
-# GSM2706547  H3K14pr_R2
-# GSM2706548  H3K14bu_R2
-# GSM2706549  Input_F1
-# GSM2706550  H3K9ac_F1
-# GSM2706551  H3K14pr_F1
-# GSM2706552  H3K14bu_F1
-# GSM2706553  Input_F2
-# GSM2706554  H3K9ac_F2
-# GSM2706555  H3K14pr_F2
-# GSM2706556  H3K14bu_F2
+
+
+
+# SRR5838434_1.fastq.gz GSM2706541  Input_R1
+# SRR5838435_1.fastq.gz GSM2706542  H3K9ac_R1
+# SRR5838436_1.fastq.gz GSM2706543  H3K14pr_R1
+# SRR5838437_1.fastq.gz GSM2706544  H3K14bu_R1
+# SRR5838438_1.fastq.gz GSM2706545  Input_R2
+# SRR5838439_1.fastq.gz GSM2706546  H3K9ac_R2
+# SRR5838440_1.fastq.gz GSM2706547  H3K14pr_R2
+# SRR5838441_1.fastq.gz GSM2706548  H3K14bu_R2
+# SRR5838442_1.fastq.gz GSM2706549  Input_F1
+# SRR5838443_1.fastq.gz GSM2706550  H3K9ac_F1
+# SRR5838444_1.fastq.gz GSM2706551  H3K14pr_F1
+# SRR5838445_1.fastq.gz GSM2706552  H3K14bu_F1
+# SRR5838446_1.fastq.gz GSM2706553  Input_F2
+# SRR5838447_1.fastq.gz GSM2706554  H3K9ac_F2
+# SRR5838448_1.fastq.gz GSM2706555  H3K14pr_F2
+# SRR5838449_1.fastq.gz GSM2706556  H3K14bu_F2
 
 ## download fastq files
 # wget https://ftp.ncbi.nlm.nih.gov/sra/reports/Metadata/SRA_Accessions.tab
@@ -68,24 +71,32 @@ do
 done
 cat *.info
 
-echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR3126242_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2047206_trim30.info # SRR3126242 GSM2047206 R-Input_ChIPSeq
-echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR3126243_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2047207_trim30.info # SRR3126243 GSM2047207 R-H4K5ac_ChIPSeq
-echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR3126244_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2047208_trim30.info # SRR3126244 GSM2047208 R-H4K5bu_ChIPSeq
-echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR3126245_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2047209_trim30.info # SRR3126245 GSM2047209 R-H4K8ac_ChIPSeq
-echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR3126246_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2047210_trim30.info # SRR3126246 GSM2047210 R-H4K8bu_ChIPSeq
-echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR3126247_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2047211_trim30.info # SRR3126247 GSM2047211 S-Input_ChIPSeq
-echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR3126248_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2047212_trim30.info # SRR3126248 GSM2047212 S-H4K5ac_ChIPSeq
-echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR3126249_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2047213_trim30.info # SRR3126249 GSM2047213 S-H4K5bu_ChIPSeq
-echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR3126250_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2047214_trim30.info # SRR3126250 GSM2047214 S-H4K8ac_ChIPSeq
-echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR3126251_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2047215_trim30.info # SRR3126251 GSM2047215 S-H4K8bu_ChIPSeq
+
+
+echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR5838434_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2706541_trim30.info # SRR5838434 GSM2706541 Input_R1
+echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR5838435_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2706542_trim30.info # SRR5838435 GSM2706542 H3K9ac_R1
+echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR5838436_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2706543_trim30.info # SRR5838436 GSM2706543 H3K14pr_R1
+echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR5838437_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2706544_trim30.info # SRR5838437 GSM2706544 H3K14bu_R1
+echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR5838438_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2706545_trim30.info # SRR5838438 GSM2706545 Input_R2
+echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR5838439_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2706546_trim30.info # SRR5838439 GSM2706546 H3K9ac_R2
+echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR5838440_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2706547_trim30.info # SRR5838440 GSM2706547 H3K14pr_R2
+echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR5838441_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2706548_trim30.info # SRR5838441 GSM2706548 H3K14bu_R2
+echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR5838442_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2706549_trim30.info # SRR5838442 GSM2706549 Input_F1
+echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR5838443_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2706550_trim30.info # SRR5838443 GSM2706550 H3K9ac_F1
+echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR5838444_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2706551_trim30.info # SRR5838444 GSM2706551 H3K14pr_F1
+echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR5838445_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2706552_trim30.info # SRR5838445 GSM2706552 H3K14bu_F1
+echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR5838446_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2706553_trim30.info # SRR5838446 GSM2706553 Input_F2
+echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR5838447_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2706554_trim30.info # SRR5838447 GSM2706554 H3K9ac_F2
+echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR5838448_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2706555_trim30.info # SRR5838448 GSM2706555 H3K14pr_F2
+echo " -U /home/fchuffar/projects/${datashare}/GSE101597/raw/SRR5838449_1_fastxtrimf30.fastq.gz " > /home/fchuffar/projects/${datashare}/GSE101597/GSM2706556_trim30.info # SRR5838449 GSM2706556 H3K14bu_F2
 
 
 ## qc align count
 # put wf on dahu and launch
 rsync -auvP ~/projects/${project}/results/${gse}/ dahu:~/projects/${project}/results/${gse}/
 cd ~/projects/${project}/results/${gse}/
-snakemake -s wf.py --cores 16 -pn
-snakemake -s wf.py --cores 49 --cluster "oarsub --project epimed -l nodes=1/core={threads},walltime=6:00:00 "  --latency-wait 30 -pn
+snakemake -s ~/projects/${project}/results/${gse}/wf.py --cores 16 -pn
+snakemake -s ~/projects/${project}/results/${gse}/wf.py --cores 49 --cluster "oarsub --project epimed -l nodes=1/core={threads},walltime=6:00:00 "  --latency-wait 60 -pn
 
 
 ## get results
