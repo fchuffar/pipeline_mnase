4f57f2fd0848ccfe0c721565d7348bdf7fdee585
Florent Chuffart <florent.chuffart@gmail.com>
2022-03-15 10:06:50 +0100

    Update geo_submission.sh

--- geo_submission/geo_submission.sh
+++ geo_submission/geo_submission.sh
@@ -1,49 +1,47 @@
-cd  ~/projects/all_tchin/results/chip_gao_novogen/
+cd ~/projects/nme2/results/chip_domenico_tgml
 source config
 ssh cargo
 
-# Metadata spreadsheet
-# https://www.ncbi.nlm.nih.gov/geo/info/examples/seq_template.xlsx
+# cd ~/projects/nme2/results/highfatdiet/geo_submission
+# wget https://www.ncbi.nlm.nih.gov/geo/info/examples/seq_template.xlsx
+
+GSE_TARGET_NAME=GSE2full 
 
 # Transfering files
 cd /home/fchuffar/projects/${datashare}/${gse}/
-ls -lha raw/*R1.fastq.gz raw/*R2.fastq.gz
-cat raw/delivered/md5.summer.txt 
+ls -lha raw/*1.fastq.gz raw/*2.fastq.gz
+# cat raw/md5sum.summer.txt
 ls -lha *.bw
 mkdir -p /bettik/chuffarf/geo_submission/${gse}/${GSE_TARGET_NAME}/fastq
 mkdir -p /bettik/chuffarf/geo_submission/${gse}/${GSE_TARGET_NAME}/bw
-rsync -auvP --copy-links \
-  /home/fchuffar/projects/${datashare}/${gse}/raw/*R1.fastq.gz \
-  /home/fchuffar/projects/${datashare}/${gse}/raw/*R2.fastq.gz \
+rsync -cauvP --copy-links \
+  /home/fchuffar/projects/${datashare}/${gse}/raw/*1.fastq.gz \
+  /home/fchuffar/projects/${datashare}/${gse}/raw/*2.fastq.gz \
   /bettik/chuffarf/geo_submission/${gse}/${GSE_TARGET_NAME}/fastq/.
 rsync -auvP --copy-links \
   /home/fchuffar/projects/${datashare}/${gse}/*.bw \
-  /bettik/chuffarf/geo_submission/${gse}/${GSE_TARGET_NAME}/bw
-
-ls -lha /bettik/chuffarf/geo_submission/${gse}/${GSE_TARGET_NAME}/*
+  /bettik/chuffarf/geo_submission/${gse}/${GSE_TARGET_NAME}/bw/.
 
 # MD5
 cd /bettik/chuffarf/geo_submission/${gse}/${GSE_TARGET_NAME}/fastq
 md5sum *.fastq.gz > md5.geo.txt
+cat md5.geo.txt | cut -f1 -d" " | sort > tmp_md5_1.txt
+cat /home/fchuffar/projects/datashare/chip_domenico_tgml/raw/fastq/md5sum.txt | cut -f1 -d" " | sort > tmp_md5_2.txt
+diff tmp_md5_1.txt tmp_md5_2.txt
+rm tmp_md5_*.txt
 cd /bettik/chuffarf/geo_submission/${gse}/${GSE_TARGET_NAME}/bw
 md5sum *.bw > md5.geo.txt
 
 
-# check MD5 fastq
-cat /home/fchuffar/projects/${datashare}/${gse}/raw/delivered/md5.summer.txt | grep cleandata | cut -f1 -d" " | sort > /tmp/md5tmp1
-cat /bettik/chuffarf/geo_submission/${gse}/${GSE_TARGET_NAME}/fastq/md5.geo.txt | cut -f1 -d" " | sort > /tmp/md5tmp2
-cat /tmp/md5tmp1
-cat /tmp/md5tmp2
-diff /tmp/md5tmp1 /tmp/md5tmp2
-
 # Put metadata
-cat /bettik/chuffarf/geo_submission/${gse}/${GSE_TARGET_NAME}/fastq/md5.geo.txt
-cat /bettik/chuffarf/geo_submission/${gse}/${GSE_TARGET_NAME}/bw/md5.geo.txt
-ls -lha /bettik/chuffarf/geo_submission/${gse}/${GSE_TARGET_NAME}/fastq/*R1.fastq.gz
-ls -lha /bettik/chuffarf/geo_submission/${gse}/${GSE_TARGET_NAME}/fastq/*R2.fastq.gz
 cd ~/projects/${project}/results/${gse}/geo_submission
-open seq_template_v10_template7_tocomplete.xlsx
-rsync -auvP seq_template_v10_template7_tocomplete.xlsx cargo:/bettik/chuffarf/geo_submission/${gse}/${GSE_TARGET_NAME}/.
+Rscript generate_metadata.R 
+
+cd ~/projects/${project}/results/${gse}/geo_submission
+rsync -auvP cargo:~/projects/${project}/results/${gse}/geo_submission/0*.xlsx ~/projects/${project}/results/${gse}/geo_submission/
+open seq_template.xlsx
+
+rsync -auvP seq_template.xlsx cargo:/bettik/chuffarf/geo_submission/${gse}/${GSE_TARGET_NAME}/.
 
 # Creating archive
 cd /bettik/chuffarf/geo_submission/${gse}/
